3df3ec41449fa9668e84f1f73322593a9af4783d
Shahriar0076 <47733699+Shahriar0076@users.noreply.github.com>
2019-10-21 09:46:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* Add your script here */
+
+
+var myVar;
+
+//function myFunction() {
+//  myVar = setInterval(alertFunc, 3000);
+//}
+
+function alertFunc() {
+  alert("Hello!");
+}
+
+function myFunction(){
+
+		
+	 console.log(document.getElementById("start").value);// start
+	 console.log(document.getElementById("end").value);// end
+	 console.log(document.getElementById("timer").value);// timer
+}
